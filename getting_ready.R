3c16d7df22fb85d5267ead24dd5d031be61c5b29
Pablo <pablovaldes@gmail.com>
2024-05-14 17:19:34 -0400

    Add comments

--- getting_ready.R
+++ getting_ready.R
@@ -1,3 +1,5 @@
 
 new_data <- read.csv("/Volumes/PVD2/1_Montreal/1_UdeM/3_Courses/Animal_Data_Science/data/test_.csv")
-summary(new_data)
+
+# Explore data characteristics 
+summary(new_data) # Summarize each variable. 
